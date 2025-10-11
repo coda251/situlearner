27d84013bc45be9634de7b9708e815b8333d193e
coda251 <poco151@outlook.com>
2025-10-11 19:43:10 +0800

    fix: update DA dict.
    It seems that DA have fixed the cert issue.

--- infra/subkit/translator/build.gradle.kts
+++ infra/subkit/translator/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.situlearner.jvm.library)
-    alias(libs.plugins.situlearner.ktor)
 }
 
 dependencies {
--- infra/subkit/translator/src/main/kotlin/com/coda/situlearner/infra/subkit/translator/ja/DAJapanese.kt
+++ infra/subkit/translator/src/main/kotlin/com/coda/situlearner/infra/subkit/translator/ja/DAJapanese.kt
@@ -4,65 +4,47 @@ import com.coda.situlearner.core.model.data.Language
 import com.coda.situlearner.core.model.data.WordMeaning
 import com.coda.situlearner.core.model.infra.WordInfo
 import com.coda.situlearner.infra.subkit.translator.Translator
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.request.get
-import io.ktor.client.statement.bodyAsText
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 import org.jsoup.nodes.Node
-import java.io.InputStream
-import java.security.KeyStore
-import java.security.cert.CertificateFactory
-import javax.net.ssl.TrustManagerFactory
-import javax.net.ssl.X509TrustManager
 
-class DAJapanese(
+internal class DAJapanese(
     override val name: String = "DA",
     override val sourceLanguage: Language = Language.Japanese,
 ) : Translator(name, sourceLanguage) {
 
-    // NOTE: this is a workaround for jsoup >= 1.21.1 which use
-    // HttpClient (do not accept sslSocketFactory) instead of HttpURLConnection,
-    // so we need to create a customized client for html query.
-    private val client by lazy {
-        this.javaClass.getResourceAsStream("/dict.asia.crt")?.let {
-            createClientWithCertificate(it)
-        } ?: HttpClient(CIO)
-    }
-
     override suspend fun fetch(word: String): List<WordInfo> {
-        val url = "https://dict.asia/jc/$word"
-        val html = client.get(url).bodyAsText()
-        val doc = Jsoup.parse(html, url)
 
-        val infos = doc.select("div#jp_comment").map { block ->
-            val wordFromWeb = block.selectFirst("span.jpword")
-                ?.text()
-                ?.trim()
-                .orEmpty()
-            val kana = block.selectFirst("span[title=假名]")
-                ?.text()
-                ?.replace("【", "")
-                ?.replace("】", "")
-                ?.trim()
-                .orEmpty()
-            val tone = block.selectFirst("span[title=音调]")
-                ?.text()
-                ?.trim()
-                .orEmpty()
-            val pronunciation = kana + tone
+        val infos = Jsoup.connect("https://dict.asia/jc/$word")
+            .get()
+            .select("div#jp_comment")
+            .map { block ->
+                val wordFromWeb = block.selectFirst("span.jpword")
+                    ?.text()
+                    ?.trim()
+                    .orEmpty()
+                val kana = block.selectFirst("span[title=假名]")
+                    ?.text()
+                    ?.replace("【", "")
+                    ?.replace("】", "")
+                    ?.trim()
+                    .orEmpty()
+                val tone = block.selectFirst("span[title=音调]")
+                    ?.text()
+                    ?.trim()
+                    .orEmpty()
+                val pronunciation = kana + tone
 
-            val span = block.select("div.jp_explain span.commentItem").firstOrNull()
-            val meanings = span?.let { parseWordMeanings(it) } ?: emptyList()
+                val span = block.select("div.jp_explain span.commentItem").firstOrNull()
+                val meanings = span?.let { parseWordMeanings(it) } ?: emptyList()
 
-            WordInfo.fromWebOrUser(
-                word = wordFromWeb,
-                dictionaryName = name,
-                pronunciations = listOf(pronunciation),
-                meanings = meanings
-            )
-        }
+                WordInfo.fromWebOrUser(
+                    word = wordFromWeb,
+                    dictionaryName = name,
+                    pronunciations = listOf(pronunciation),
+                    meanings = meanings
+                )
+            }
         return infos
     }
 
@@ -103,28 +85,4 @@ class DAJapanese(
 
         return meanings
     }
-}
-
-private fun createClientWithCertificate(certInputStream: InputStream): HttpClient {
-    val certFactory = CertificateFactory.getInstance("X.509")
-    val caCert = certFactory.generateCertificate(certInputStream)
-
-    val ks = KeyStore.getInstance(KeyStore.getDefaultType()).apply {
-        load(null, null)
-        setCertificateEntry("missingCA", caCert)
-    }
-
-    val tm = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
-        .apply { init(ks) }
-        .trustManagers.filterIsInstance<X509TrustManager>().firstOrNull()
-
-    val client = HttpClient(CIO) {
-        engine {
-            https {
-                trustManager = tm
-            }
-        }
-    }
-
-    return client
 }
